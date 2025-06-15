dc0af3c8af94e8723dc9e477104200ef76f3d8a7
rafaelputra <rafaelputra1337@gmail.com>
2025-06-15 23:13:20 +0700

    Menambahkan style

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor
 import joblib
+import plotly.graph_objects as go
 
 model_rfr = joblib.load("train/models/rfr.pkl")
 
@@ -38,13 +39,31 @@ with col1:
 
 with col2:
     if predict_button:
+        st.subheader("Hasil Metode Random Forest Regression :chart_with_upwards_trend:", divider=True)
         rows = st.container(border=True)
         features = np.array([[spx,uso,slv,eur_usd, gld, gld_t1]])
         prediction_rfr = model_rfr.predict(features)
-        rows.write(f"Prediksi harga emas dengan Random Forest Regression: {prediction_rfr[0]:.2f} USD")
+        rows.write(f"Prediksi harga emas besok dengan Random Forest Regression: **{prediction_rfr[0]:.2f} USD**")
         _, status, selisih = prediksi_harga_emas_lengkap(model_rfr, features[0], gld)
-        rows.write(f"Status: {status} sebesar {selisih:.2f} USD dibanding harga saat ini {gld:.2f} USD\n")
+        rows.write(f"**Status**: {status} sebesar **{selisih:.2f} USD** dibanding harga saat ini **{gld:.2f} USD**\n")
+
+        st.subheader("Hasil XGBoost :chart_with_upwards_trend:", divider="red")
         prediction_xgb = model_xgb.predict(features)
         _, status, selisih = prediksi_harga_emas_lengkap(model_xgb, features[0], gld)
-        rows.write(f"Prediksi harga emas dengan XGBoost: {prediction_xgb[0]:.2f} USD")
-        rows.write(f"Status: {status} sebesar {selisih:.2f} USD dibanding harga saat ini {gld:.2f} USD\n")
+        rows.write(f"Prediksi harga emas besok dengan XGBoost: **{prediction_xgb[0]:.2f} USD**")
+        rows.write(f"**Status**: {status} sebesar **{selisih:.2f} USD** dibanding harga saat ini **{gld:.2f} USD**\n")
+
+        st.subheader("Visualisasi :bar_chart:", divider="green")
+        fig = go.Figure(data=[
+            go.Bar(name='Harga Hari Ini', x=['Harga'], y=[harga_emas_hari_ini], marker_color='gray'),
+            go.Bar(name='Random Forest', x=['Harga'], y=[pred_rf], marker_color='forestgreen'),
+            go.Bar(name='XGBoost', x=['Harga'], y=[pred_xgb], marker_color='orange'),
+        ])
+
+        fig.update_layout(
+            title="Perbandingan Harga Emas Hari Ini dan Prediksi (+1 Hari)",
+            yaxis_title="Harga (USD)",
+            barmode='group'
+        )
+
+        st.plotly_chart(fig)
